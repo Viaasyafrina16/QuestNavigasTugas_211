8f0d4a7505d82492d89dc69f318848372cc4a393
Viaasyafrina16 <syafrinavia@gmail.com>
2025-11-06 22:17:43 +0700

    membuat form untuk beranda

---
+++ app/src/main/java/com/example/navigasitugas/view/Beranda.kt
@@ -0,0 +1,23 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
+package com.example.navigasitugas.view
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.navigasiku.R
+
+@Composable
+fun Beranda(
+    onSubmitClick: () -> Unit
+) {
+
