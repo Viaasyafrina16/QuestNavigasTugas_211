e4a60d6db64c0f648939a0258f36318b5413b7eb
Viaasyafrina16 <syafrinavia@gmail.com>
2025-11-07 15:04:54 +0700

    menambahkan state dan memanggil form datadiri

--- app/src/main/java/com/example/navigasitugas/Navigasi.kt
+++ app/src/main/java/com/example/navigasitugas/Navigasi.kt
@@ -3,11 +3,18 @@ package com.example.navigasitugas
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.navigasitugas.view.Beranda
+import com.example.navigasitugas.view.FormDataDiri
+import com.example.navigasitugas.view.TampilData
 
 enum class Navigasi{
     Beranda,
@@ -19,12 +26,20 @@ enum class Navigasi{
 fun DataApp(
     navController: NavHostController = rememberNavController(),
 
-    modifier: Modifier
+  modifier:  Modifier
+
 ) {
+
+    var nama by remember { mutableStateOf("") }
+    var jk by remember { mutableStateOf("") }
+    var status by remember { mutableStateOf("") }
+    var alamat by remember { mutableStateOf("") }
+
     Scaffold { isiRuang ->
         NavHost(
             navController = navController,
             startDestination = Navigasi.Beranda.name,
+
             modifier = Modifier.padding(isiRuang)
         ) {
             composable(route = Navigasi.Beranda.name) {
@@ -36,14 +51,22 @@ fun DataApp(
             }
             composable(route = Navigasi.Formulir.name) {
                 FormDataDiri(
-                    OnSubmitBtnClick = {
+                    onSubmit = { n, j, s, a ->
+                        nama = n
+                        jk = j
+                        status = s
+                        alamat = a
                         navController.navigate(Navigasi.Detail.name)
                     }
                 )
             }
             composable(Navigasi.Detail.name) {
                 TampilData(
-                    onBerandaClick = {
+                    nama = nama,
+                    jk = jk,
+                    status = status,
+                    alamat = alamat,
+                    onBackBtnClick = {
                         navController.navigate(Navigasi.Beranda.name) {
                             popUpTo(Navigasi.Beranda.name) { inclusive = true }
                         }
@@ -56,6 +79,7 @@ fun DataApp(
         }
     }
 }
+
 
 
 
